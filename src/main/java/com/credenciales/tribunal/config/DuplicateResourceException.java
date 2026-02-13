6db0fa9d8690e6c91bf52a8f488dcba9a456fc1d
tribu <tribup333@gmail.com>
2026-02-13 12:44:24 -0400

    funciona corre geh daba error

--- src/main/java/com/credenciales/tribunal/config/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.credenciales.tribunal.config;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
