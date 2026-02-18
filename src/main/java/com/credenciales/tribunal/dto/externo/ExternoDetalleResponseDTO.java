514b89628546ab68a23b8c706c49b6a122dc96da
tribu <tribup333@gmail.com>
2026-02-18 18:05:45 -0400

    creando dtos externos

---
+++ src/main/java/com/credenciales/tribunal/dto/externo/ExternoDetalleResponseDTO.java
@@ -0,0 +1,28 @@
+package com.credenciales.tribunal.dto.externo;
+
+import com.credenciales.tribunal.model.enums.TipoExterno;
+import com.credenciales.tribunal.dto.image.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExternoDetalleResponseDTO {
+
+    private Long id;
+    private String nombreCompleto;
+    private String carnetIdentidad;
+    private String identificador;
+    private String orgPolitica;
+    private TipoExterno tipoExterno;
+    private LocalDateTime createdAt;
+    private ImagenBasicaDTO imagen;
+    //private List<AsignacionQrBasicaDTO> asignaciones; // DTO básico de asignaciones
+}
---
+++ src/main/java/com/credenciales/tribunal/dto/externo/ExternoDto.java
@@ -0,0 +1,28 @@
+package com.credenciales.tribunal.dto.externo;
+
+import com.credenciales.tribunal.model.enums.TipoExterno;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExternoDto {
+
+    private Long id;
+    private String nombreCompleto;
+    private String carnetIdentidad;
+    private String identificador;
+    private String orgPolitica;
+    private TipoExterno tipoExterno;
+    private LocalDateTime createdAt;
+    private Long idImagen;
+    //private List<AsignacionQrBasicaDTO> asignaciones; // DTO básico de asignaciones
+}
+
