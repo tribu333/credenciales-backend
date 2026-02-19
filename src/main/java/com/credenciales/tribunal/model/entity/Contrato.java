2729edc171aa248a3c099c4c184f991e43a1975b
Josue Brandon Rodriguez Blanco <josue.rodriguez@oep.net>
2026-02-19 17:13:11 -0400

    feat: new entity for contrato

---
+++ src/main/java/com/credenciales/tribunal/model/entity/Contrato.java
@@ -0,0 +1,44 @@
+Contrato.java
+package com.credenciales.tribunal.model.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contrato")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Contrato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "personal_id", nullable = false)
+    private Personal personal;
+
+	@Column(nullable = false, columnDefinition = "TINYINT DEFAULT 1")
+    @Builder.Default
+    private Boolean activo = true;
+    
+    @Column(nullable = false, length = 20) 
+    @Builder.Default
+    private String tipo;
+
+    @Column(name = "fecha_inicio", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime fechaInicio;
+
+    @Column(name = "fecha_fin")
+    private LocalDateTime fechaFin;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cargo_id", nullable = false)
+    private Cargo cargo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "proceso_id")
+    private ProcesoElectoral proceso;
+
+}
