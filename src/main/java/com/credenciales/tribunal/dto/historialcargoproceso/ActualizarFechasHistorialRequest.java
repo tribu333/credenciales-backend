5d5b8a6b0f63732317b09a015e8eb20953b07059
frd <tribup333@gmail.com>
2026-03-01 01:03:23 -0300

    format

--- src/main/java/com/credenciales/tribunal/dto/historialcargoproceso/ActualizarFechasHistorialRequest.java
+++ src/main/java/com/credenciales/tribunal/dto/historialcargoproceso/ActualizarFechasHistorialRequest.java
@@ -2,6 +2,8 @@ package com.credenciales.tribunal.dto.historialcargoproceso;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,8 +22,10 @@ public class ActualizarFechasHistorialRequest {
     private Long cargoProcesoId;
     
     @NotNull(message = "La fecha de inicio es requerida")
+    //@JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDateTime fechaInicio;
     
     @NotNull(message = "La fecha de fin es requerida")
+    //@JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDateTime fechaFin;
 }
