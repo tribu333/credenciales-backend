e698e335c43b49a6c77d41199866d4e201f51516
tribu <tribup333@gmail.com>
2026-04-17 23:37:47 -0400

    http de vps

--- src/main/java/com/credenciales/tribunal/config/OpenApiConfig.java
+++ src/main/java/com/credenciales/tribunal/config/OpenApiConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.info.Contact;
 @Configuration
 public class OpenApiConfig{
@@ -17,7 +18,15 @@ public class OpenApiConfig{
                 .title("Control Api")
                 .version("1.0")
                 .description("Api for ")
-                .contact( new Contact().name("Alfred Lazar").email("micorreo@example.com").url(""))
+                .contact( new Contact().name("Alfred L, Elizabeth B").email("micorreo@example.com").url(""))
+            )
+            .addServersItem(new Server()
+                .url("https://api.j0o88kckww4cos8cgog80wsw.158.220.117.118.sslip.io")
+                .description("Servidor de producción HTTPS")
+            )
+            .addServersItem(new Server()
+                .url("http://localhost:8085")
+                .description("Servidor local")
             )
             .addSecurityItem(new SecurityRequirement()
                 .addList("bearerAuth"))
